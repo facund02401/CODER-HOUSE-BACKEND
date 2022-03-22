e49408f37599efa5b9c84e42c768696bf9d1d311
Facundo Rodriguez Orge <facundorodriguez2401@gmail.com>
2022-03-21 23:39:10 -0300

    Primera entrega

---
+++ README.md
@@ -0,0 +1,4 @@
+# Notas del curso de Backend - Docente Mariano Aquino
+
+Link al repo del curso:
+https://github.com/drmaquino/ch-backend-30935
---
+++ Tarea_1_Clases.js
@@ -0,0 +1,50 @@
+class Usuario {
+  constructor(nombre, apellido, libros = [], mascotas = []) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.libros = libros;
+    this.mascotas = mascotas;
+  }
+
+  getFullName() {
+    console.log(`${this.nombre} ${this.apellido}`);
+  }
+
+  addMascota(masc) {
+    this.mascotas.push(masc);
+  }
+
+  countMascotas() {
+    console.log(this.mascotas.length);
+  }
+
+  addBook(nombre, autor) {
+    this.libros.push({ nombre: nombre, autor: autor });
+  }
+
+  getBookNames() {
+    const arrayNombresLibros = [];
+    for ( const cadaLibro in this.libros){
+        arrayNombresLibros.push(this.libros[cadaLibro]['nombre'])
+    }
+    console.log(arrayNombresLibros);
+  }
+}
+
+const usuario = new Usuario(
+  "facundo",
+  "rodriguez",
+  [{ nombre: 1984, autor: "Orwell" }],
+  ["acacia"]
+);
+
+
+console.log(usuario);
+usuario.getFullName();
+usuario.countMascotas();
+usuario.getBookNames();
+
+usuario.addMascota('gato');
+usuario.addBook('Rayuela', 'Cortazar');
+usuario.countMascotas();
+usuario.getBookNames();
